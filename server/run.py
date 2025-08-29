44c7119338271795989a95bf47fd87da9f5fe517
Rhydam Kapoor <rhydamkapoor@gmail.com>
2025-08-29 11:21:19 +0530

    Changed port setting

--- server/run.py
+++ server/run.py
@@ -1,7 +1,8 @@
-from app import create_app;
+import os
+from app import create_app
 
-
-app = create_app();
+app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=app.config['DEBUG']);
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=app.config['DEBUG'])
